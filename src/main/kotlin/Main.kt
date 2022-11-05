4dac7bd5e577c288c1412e76397cab133d3e5877
Griefed <griefed@griefed.de>
2022-11-05 15:21:51 +0100

    no message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    var min = args[0].toInt()
+    var max = args[1].toInt()
+    val target = args[2].toInt()
+    var guesses = 0
+    var guess = Random.nextInt(min,max)
+    println("Min: $min | Max: $max | Target: $target | Guess: $guess")
+    while(true) {
+        if (guess == target) {
+            break
+        } else if (guess < target) {
+            println("Guessed $guess which is smaller than $target.")
+            min = guess
+            guesses++
+            guess = Random.nextInt(min,max)
+        } else {
+            println("Guessed $guess which is bigger than $target.")
+            max = guess
+            guesses++
+            guess = Random.nextInt(min,max)
+        }
+    }
+    println("It took $guesses guesses to reach $target")
 }
